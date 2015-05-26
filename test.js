bb37c452398419ef1ff9cf7e3a4cfdc151a5a4bc
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-26 18:20:32 -0400

    Updated test case

--- test.js
+++ test.js
@@ -2,18 +2,25 @@
 var assert = require('assert');
 var double_quotes = require('./');
 
-it('should convert matching single-quotes to double-quotes', function()
-{
-  assert.equal(double_quotes(''), '');
-	assert.equal(double_quotes('foo'), 'foo');
-	assert.equal(double_quotes('\'\''), '""');
-	assert.equal(double_quotes('""'), '""');
-	assert.equal(double_quotes('\'foo\''), '"foo"');
-	assert.equal(double_quotes('"foo"'), '"foo"');
-	assert.equal(double_quotes('bar "foo" baz'), 'bar "foo" baz');
-	assert.equal(double_quotes('\'bar\' "foo" \'baz\''), '"bar" "foo" "baz"');
-	assert.equal(double_quotes('\\\'foo\\\''), '"foo"');
-	assert.equal(double_quotes('{\'a\':\'<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': '<a href=\'addr\'>'}));
-	assert.equal(double_quotes('{\'a\':\'aa\\n<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': 'aa\n<a href=\'addr\'>'}));
-	assert.equal(double_quotes(JSON.stringify({'a': 'b""c' })), '{"a":"b\\\"\\\"c"}');
+/*
+Function to test the Double_Quotes converts a literal with single-quotes to double-quotes
+*/
+describe('Double_Quotes', function(){
+  describe('#convertSingleQuotesToDoubleQuotes()', function(){
+    it('should convert matching single-quotes to double-quotes', function()
+    {
+      assert.equal(double_quotes(''), '');
+    	assert.equal(double_quotes('foo'), 'foo');
+    	assert.equal(double_quotes('\'\''), '""');
+    	assert.equal(double_quotes('""'), '""');
+    	assert.equal(double_quotes('\'foo\''), '"foo"');
+    	assert.equal(double_quotes('"foo"'), '"foo"');
+    	assert.equal(double_quotes('bar "foo" baz'), 'bar "foo" baz');
+    	assert.equal(double_quotes('\'bar\' "foo" \'baz\''), '"bar" "foo" "baz"');
+    	assert.equal(double_quotes('\\\'foo\\\''), '"foo"');
+    	assert.equal(double_quotes('{\'a\':\'<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': '<a href=\'addr\'>'}));
+    	assert.equal(double_quotes('{\'a\':\'aa\\n<a href=\\\'addr\\\'>\'}'), JSON.stringify({'a': 'aa\n<a href=\'addr\'>'}));
+    	assert.equal(double_quotes(JSON.stringify({'a': 'b""c' })), '{"a":"b\\\"\\\"c"}');
+    })
+  })
 });
