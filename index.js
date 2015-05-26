2ed99e039948637c625b86c1103a4fa3468a08fb
Rahul Nadella <rahulnadella@yahoo.com>
2015-05-26 18:24:56 -0400

    Updated the replace function

--- index.js
+++ index.js
@@ -2,10 +2,14 @@
 
 module.exports = function (str)
 {
-  return str.replace(/(?:\\*)?'([^'\\]*\\.)*[^']*'/g, function (match) {
+  /*
+  Replace the single-quotes with double quotes using replace function
+  */
+  return str.replace(/(?:\\*)?'([^'\\]*\\.)*[^']*'/g, function (match)
+  {
           return match
-            			.replace(/\\'/g, '\'')           // unescape single-quotes
-            			.replace(/([^\\])"/g, '$1\\\"')  // escape double-quotes
-            			.replace(/^'|'$/g, '"');         // convert
+            			.replace(/\\'/g, '\'')
+            			.replace(/([^\\])"/g, '$1\\\"')
+            			.replace(/^'|'$/g, '"');
   });
 };
